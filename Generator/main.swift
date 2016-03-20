c19467d1317a20377111642d4165527ebe6ea8cb
Ryohei Ikegami <iofg2100@gmail.com>
2016-03-20 17:38:41 +0900

    Dump cursor kind

--- Generator/main.swift
+++ Generator/main.swift
@@ -9,6 +9,13 @@ func visitClasses(cursor: CXCursor, parent: CXCursor, clientData: CXClientData) 
   return CXChildVisit_Recurse
 }
 
+func visitDump(cursor: CXCursor, parent: CXCursor, clientData: CXClientData) -> CXChildVisitResult {
+  let kindStr = clang_getCursorKindSpelling(clang_getCursorKind(cursor))
+  print(String.fromCString(clang_getCString(kindStr)) ?? "")
+  clang_disposeString(kindStr)
+  return CXChildVisit_Recurse
+}
+
 let index = clang_createIndex(0, 1)
 let args = [String]()
 let translationUnit = clang_createTranslationUnitFromSourceFile(
@@ -19,6 +26,7 @@ let translationUnit = clang_createTranslationUnitFromSourceFile(
   0, nil
 )
 let rootCursor = clang_getTranslationUnitCursor(translationUnit)
+clang_visitChildren(rootCursor, visitDump, nil)
 clang_visitChildren(rootCursor, visitClasses, nil)
 
 clang_disposeTranslationUnit(translationUnit)
