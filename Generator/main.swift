ff9adfc2894c71fa952aca4242d00efde33424c0
Ryohei Ikegami <iofg2100@gmail.com>
2016-03-20 14:28:41 +0900

    Test visiting

--- Generator/main.swift
+++ Generator/main.swift
@@ -1,12 +1,22 @@
 import CClang
 
+func visit(cursor: CXCursor, parent: CXCursor, clientData: CXClientData) -> CXChildVisitResult {
+  let name = clang_getCursorSpelling(cursor)
+  print(String.fromCString(clang_getCString(name)) ?? "")
+  return CXChildVisit_Recurse
+}
+
 let index = clang_createIndex(0, 1)
 let args = [String]()
 let translationUnit = clang_createTranslationUnitFromSourceFile(
   index,
-  "./unbrella.cpp",
+  Process.arguments[1],
   Int32(CStringArray(args).length),
   CStringArray(args).constPointers,
   0, nil
 )
+let rootCursor = clang_getTranslationUnitCursor(translationUnit)
+clang_visitChildren(rootCursor, visit, nil)
+
 clang_disposeTranslationUnit(translationUnit)
+clang_disposeIndex(index)
