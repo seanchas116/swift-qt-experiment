9d4c00b9ad6cd87def9fab124b15a2872c23d768
Ryohei Ikegami <iofg2100@gmail.com>
2016-03-20 19:24:13 +0900

    CXUtil.swift

--- Generator/main.swift
+++ Generator/main.swift
@@ -1,46 +1,5 @@
 import CClang
 
-class ClangString: CustomStringConvertible {
-  let data: CXString
-  init(_ data: CXString) {
-    self.data = data
-  }
-
-  var description: String {
-    return String.fromCString(clang_getCString(data)) ?? ""
-  }
-
-  deinit {
-    clang_disposeString(data)
-  }
-}
-
-extension CXType: CustomStringConvertible {
-  public var description: String {
-    return ClangString(clang_getTypeSpelling(self)).description
-  }
-}
-
-extension CXCursor: CustomStringConvertible {
-  var type: CXType {
-    return clang_getCursorType(self)
-  }
-
-  var kind: CXCursorKind {
-    return clang_getCursorKind(self)
-  }
-
-  public var description: String {
-    return ClangString(clang_getCursorSpelling(self)).description
-  }
-}
-
-extension CXCursorKind: CustomStringConvertible {
-  public var description: String {
-    return ClangString(clang_getCursorKindSpelling(self)).description
-  }
-}
-
 func visitMethods(cursor: CXCursor, parent: CXCursor, clientData: CXClientData) -> CXChildVisitResult {
   let kind = cursor.kind
   if kind == CXCursor_CXXMethod {
